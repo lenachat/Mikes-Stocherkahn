a0b9d2b7c5f13ce92cee93942ec45ba72341317e
lenachat <124374456+lenachat@users.noreply.github.com>
2025-03-06 12:01:09 +0100

    horizontal scroll of 'wir' section

--- mikes-stocherkahn.js
+++ mikes-stocherkahn.js
@@ -1,29 +1,59 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger)
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   gsap.registerPlugin(ScrollTrigger)
 
-  const races = document.querySelector(".races");
+//   const gallery = document.querySelector(".gallery");
+
+//   function getScrollAmount() {
+//     let galleryWidth = gallery.scrollWidth;
+//     return -(galleryWidth - window.innerWidth);
+//   }
+
+//   const tween = gsap.to(gallery, {
+//     x: getScrollAmount(),
+//     duration: 3,
+//     ease: "none",
+//   })
+
+//   ScrollTrigger.create({
+//     trigger: ".gallery-wrapper",
+//     start: "top 20%",
+//     end: () => `+=${getScrollAmount() * -1}`,
+//     pin: true,
+//     scrub: 1,
+//     animation: tween,
+//     invalidateOnRefresh: true,
+//     markers: true,
+//   });
+
+// });
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const gallery = document.querySelector(".gallery");
 
   function getScrollAmount() {
-    let racesWidth = races.scrollWidth;
-    return -(racesWidth - window.innerWidth);
+    let galleryWidth = gallery.scrollWidth;
+    let windowWidth = window.innerWidth;
+    return galleryWidth - windowWidth;
   }
 
-  const tween = gsap.to(races, {
-    x: getScrollAmount(),
+  const scrollAmount = getScrollAmount();
+
+  const tween = gsap.to(gallery, {
+    x: -scrollAmount,
     duration: 3,
-    ease: "linear",
-  })
+    ease: "none",
+  });
 
   ScrollTrigger.create({
-    trigger: "#fahrten",
-    start: "top 20%",
-    end: () => `+=${getScrollAmount() * -1}`,
+    trigger: "#wir",
+    start: "top 10%",
+    end: () => `+=${scrollAmount}`,
     pin: true,
     scrub: 1,
     animation: tween,
-    invalidateOnRefresh: true,
-    markers: true,
+    invalidateOnRefresh: true, // Recalculate on resize
+    markers: false,
   });
-
 });
-
