e66227d75c47ea46f92ae69347f54a6d26e7d89f
lenachat <124374456+lenachat@users.noreply.github.com>
2025-03-13 12:43:29 +0100

    about animation - 3

--- mikes-stocherkahn.js
+++ mikes-stocherkahn.js
@@ -83,32 +83,152 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
   });
 
+  // ----- About Section Animation ----- new version
+
+  const boat = gsap.utils.toArray(".boat-container-section");
+  const textParagaraphs = gsap.utils.toArray(".text-paragraph");
+
+  // gsap.set(textParagaraphs, { yPercent: 101 }); // Position images outside the view initially
+  gsap.set(textParagaraphs, { autoAlpha: 0 });
+  const allParagraphs = gsap.utils.toArray(".text-paragraph");
+
+  let mm2 = gsap.matchMedia();
+
+  // When viewport is at least 600px wide, apply desktop functionality
+  mm2.add("(min-width: 600px)", () => {
+
+    // Pin the right side container as you scroll
+    gsap.to(".right-2", {
+      scrollTrigger: {
+        trigger: ".gallery-2",
+        start: "top top",
+        end: "bottom bottom",
+        pin: ".right-2",
+        pinSpacing: true, // Disable additional spacing
+      }
+    });
+
+    // Create scrolltrigger for each text section
+    boat.forEach((detail2, index) => {
+      let headline2 = detail2.querySelector("img");
+      let animation2 = gsap.timeline()
+        .to(textParagaraphs[index], { autoAlpha: 1 }); // Move image into view
+        // .set(allParagraphs[index], { autoAlpha: 0 }); // Fade out previous image
+
+      ScrollTrigger.create({
+        trigger: headline2,
+        start: "top 70%",
+        end: "top 30%",
+        animation: animation2,
+        scrub: 1,
+        markers: true,
+        pinSpacing: true,
+      });
+    });
+  });
+
+  // ----- About Section Animation ----- new version 2
+
+  // gsap.to(".boat-container", {
+  //   y: () => -(document.querySelector(".boat-container").offsetHeight - window.innerHeight),
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: ".left-2",
+  //     start: "top top",
+  //     end: "bottom bottom",
+  //     scrub: true,
+  //     pin: ".left-2"
+  //   }
+  // });
+
+  // // const boat = gsap.utils.toArray(".boat-container-section:not(:first-child)");
+  // const textParagaraphs = gsap.utils.toArray(".text-paragraph");
+
+  // gsap.set(textParagaraphs, { yPercent: 101 }); // Position images outside the view initially
+  // // const allParagraphs = gsap.utils.toArray(".text-paragraph");
+
+  // let mm2 = gsap.matchMedia();
+
+  // // When viewport is at least 600px wide, apply desktop functionality
+  // mm2.add("(min-width: 600px)", () => {
+
+  //   textParagaraphs.forEach((text, index) => {
+  //     const imageContainer = document.querySelector(".boat-container");
+  //     const imgHeight = imageContainer.offsetHeight;
+  //     const totalSections = textParagraphs.length;
+
+  //     const triggerPoint = (index + 1) * (imgHeight / (totalSections + 1));
+
+  //     ScrollTrigger.create({
+  //       trigger: ".left-2",
+  //       start: () => {
+  //         // Startpunkt: Vom oberen Rand der linken Spalte plus triggerPoint, minus die halbe Fensterhöhe
+  //         const leftTop = document.querySelector(".left-2").getBoundingClientRect().top + window.scrollY;
+  //         return leftTop + triggerPoint - window.innerHeight / 2;
+  //       },
+  //       end: "+=1", // Minimaldauer, da nur der Moment relevant ist
+  //       onEnter: () => gsap.to(para, { yPercent: 0, autoAlpha: 1, duration: 1 }),
+  //       onLeaveBack: () => gsap.to(para, { yPercent: 101, autoAlpha: 0, duration: 1 }),
+  //       markers: true // Zum Debuggen – zum späteren Entfernen
+  //     });
+
+  // -------------------------------------------------
+  // // Pin the right side container as you scroll
+  // gsap.to(".right-2", {
+  //   scrollTrigger: {
+  //     trigger: ".gallery-2",
+  //     start: "top top",  
+  //     end: "bottom bottom",
+  //     pin: ".right-2",
+  //     pinSpacing: true, // Disable additional spacing
+  //   }
+  // });
+
+  // // Create scrolltrigger for each text section
+  // boat.forEach((detail2, index) => {
+  //   let headline2 = detail2.querySelector("img");
+  //   let animation2 = gsap.timeline()
+  //     .to(textParagaraphs[index], { yPercent: 0 }) // Move image into view
+  //     .set(allParagraphs[index], { autoAlpha: 0 }); // Fade out previous image
+
+  // ScrollTrigger.create({
+  //   trigger: headline2,
+  //   start: "top 70%",
+  //   end: "top 30%",
+  //   animation: animation2,
+  //   scrub: 1,
+  //   markers: true,
+  //   pinSpacing: true,
+  // });
+// });
+//   });
+
   // ----- About Section Animation -----
 
-  gsap.to('#about', {
-    scrollTrigger: {
-      trigger: "#about",
-      start: "top top",
-      end: "bottom bottom",
-      scrub: 1,
-      markers: false,
-      pinSpacing: false,
-      pin: true,
-    },
-  })
+  // gsap.to('#about', {
+  //   scrollTrigger: {
+  //     trigger: "#about",
+  //     start: "top top",
+  //     end: "bottom bottom",
+  //     scrub: 1,
+  //     markers: false,
+  //     pinSpacing: false,
+  //     pin: true,
+  //   },
+  // })
 
-  gsap.to(".kahn-topview", {
-    y: "-100vh",
-    duration: 2,
-    scrollTrigger: {
-      trigger: "#about",
-      start: "top top",
-      end: "bottom bottom",
-      scrub: true,
-      markers: false,
-      pinSpacing: false,
-    }
-  });
+  // gsap.to(".kahn-topview", {
+  //   y: "-100vh",
+  //   duration: 2,
+  //   scrollTrigger: {
+  //     trigger: "#about",
+  //     start: "top top",
+  //     end: "bottom bottom",
+  //     scrub: true,
+  //     markers: false,
+  //     pinSpacing: false,
+  //   }
+  // });
 
   // gsap.to(".text-section-1", {
   //   autoAlpha: 1,
